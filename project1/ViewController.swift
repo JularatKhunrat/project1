681a15478a1423ee66b53fb63b01353288dc3cc8
cherrystz <pharuthapol.junpet@g.swu.ac.th>
2020-10-27 14:49:11 +0700

    Initial Commit

---
+++ project1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  project1
+//
+//  Created by jularat khunrat on 27/10/2563 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
